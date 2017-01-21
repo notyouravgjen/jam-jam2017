f815bf27d1b4216e68edadef431c02754070d773
Angela <angelapennwu@gmail.com>
2017-01-21 11:27:27 -0500

    Added test wave scene, Waveable class, WaterLogs class

---
+++ sandcastle/Assets/WaterLogs.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterLogs : MonoBehaviour {
+    public int size = 50;
+    public Transform waveObject;
+    public float waveSpacing;
+    private Waveable waveNumbers;
+    private Transform[] waves;
+
+	// Use this for initialization
+	void Start () {
+        waveNumbers = new Waveable(size);
+        waves = new Transform[size];
+        for(int i=0; i < size; i++)
+        {
+            float xOffset = i * waveSpacing;
+            waves[i] = Instantiate(waveObject, new Vector3(transform.position.x + xOffset, transform.position.y, transform.position.z), Quaternion.Euler(90, 0, 0), transform);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        waveNumbers.Update();
+		for(int i=0; i<size; i++)
+        {
+            Vector3 wavePos = waves[i].transform.position;
+            wavePos.y = transform.position.y + waveNumbers.getOffset(i);
+            waves[i].transform.position = wavePos;
+        }
+	}
+}
---
+++ sandcastle/Assets/Waveable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waveable {
+
+    public int size;
+    private float[] heightOffsetsTravelingRight; // Delta height from defaultHeight that should moving right
+    private float[] heightOffsetsTravelingLeft;  // Delta height from defaultHeight that is moving left
+
+	//Constructor
+	public Waveable (int size) {
+        this.size = size;
+        heightOffsetsTravelingRight = new float[size];
+        heightOffsetsTravelingLeft = new float[size];
+    }
+	
+	// Update should be called once per frame manually
+	public void Update () {
+		
+	}
+
+    public float getOffset(int index)
+    {
+        return heightOffsetsTravelingLeft[index] + heightOffsetsTravelingRight[index];
+    }
+}
