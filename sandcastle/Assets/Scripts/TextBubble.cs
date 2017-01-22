ca098a0e3fb07125aa78b24c7172ec958f4ec975
Theirah <theirahhkhah@gmail.com>
2017-01-22 15:59:15 -0500

    Text will display

--- sandcastle/Assets/Scripts/TextBubble.cs
+++ sandcastle/Assets/Scripts/TextBubble.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class TextBubble : MonoBehaviour {
 
-    public Texture backgroundBubble;
+    public GameObject backgroundBubble;
     public string text;
     private GUIStyle textStyle;
-    private Rect positionAndSize;
-    public ActivatedTextBubbles.Trigger triggersOn; 
+    public float timeDisplayedInSec=2;
+    public float upOffset;
+    public float rightOffset;
+    public float forwardOffset=1.5f;
+    public float width=.25f;
+    public float height=.25f;
+    public ActivatedTextBubbles.Trigger triggersOn;
+    public float textOffset = -.2f;
+    public float textXRotate, textYRotate = 90, textZRotate;
+    private GameObject displayedBubble;
 
     // Use this for initialization
     void Start () {
@@ -22,7 +30,18 @@ public class TextBubble : MonoBehaviour {
 
     public void Display()
     {
-        GUI.Label(positionAndSize, backgroundBubble);
-        GUI.Label(positionAndSize, text, textStyle);
+        if (displayedBubble == null)
+        {
+            displayedBubble = Instantiate(backgroundBubble, new Vector3(transform.position.x - forwardOffset, transform.position.y + upOffset, transform.position.z - rightOffset), Quaternion.Euler(90, 0, 90), transform);
+            displayedBubble.transform.localScale = new Vector3(width, 1, height);
+            GameObject textMeshObject = new GameObject("TextMeshObject");
+            TextMesh textMesh = textMeshObject.AddComponent<TextMesh>();
+            textMesh.text = text;
+            textMeshObject.transform.SetParent(displayedBubble.transform, false);
+            textMesh.transform.rotation = Quaternion.Euler(textXRotate, textYRotate, textZRotate);
+            textMesh.offsetZ = textOffset;
+            textMesh.color = Color.black;
+            Destroy(displayedBubble, timeDisplayedInSec);
+        }
     }
 }
