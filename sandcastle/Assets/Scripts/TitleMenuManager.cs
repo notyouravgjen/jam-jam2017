67af38dec48f48204a4c400496b4d78261c3f99a
jas2715 <jas2715@rit.edu>
2017-01-22 12:41:49 -0500

    Title Menu music timing

--- sandcastle/Assets/Scripts/TitleMenuManager.cs
+++ sandcastle/Assets/Scripts/TitleMenuManager.cs
@@ -5,17 +5,45 @@ using UnityEngine.SceneManagement;
 
 public class TitleMenuManager : MonoBehaviour {
 
+	public AudioSource splashSound;
+	public AudioSource music;
+
+	private bool fadeMusic = false;
+	private bool hasSplashed = false;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		// Fade music and transition to next scene
+		if (fadeMusic)
+		{
+			music.volume -= 0.01f;
+
+			if (!hasSplashed && music.volume <= 0.75f)
+			{
+				splashSound.Play();
+				hasSplashed = true;
+			}
+
+			if (music.volume <= 0)
+			{
+				fadeMusic = false;
+				Invoke("PlayGame", 2.5f);
+			}
+		}
 	}
 
 	public void OnPlayClicked()
+	{
+		fadeMusic = true;
+	}
+
+	private void PlayGame()
 	{
 		SceneManager.LoadScene("Waves");
 	}
